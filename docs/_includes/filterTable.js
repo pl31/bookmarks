43b392be163d63667b970c26f4a13608bb373e86
Patrick Büch <git@paco.pl31.de>
2017-11-19 11:30:45 +0100

    Try get tag list

--- docs/_includes/filterTable.js
+++ docs/_includes/filterTable.js
@@ -1,22 +1,30 @@
 function filterTable() {
   // Declare variables
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("tagFilterForm");
+  var form, input, tagList = [];
+  form = document.getElementById("tagFilterForm");
+  input = form.getElementsByTagName("input");
 
-  return;
-  filter = input.value.toUpperCase();
-  table = document.getElementById("allBookmarksTable");
-  tr = table.getElementsByTagName("tr");
 
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[2];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
+  for (i = 0; i < input.length; i++) {
+    if (input[i].checked) {
+        tagList.push(input[i].value.toLowerCase());
     }
   }
+
+  return;
+  // filter = input.value.toUpperCase();
+  // table = document.getElementById("allBookmarksTable");
+  // tr = table.getElementsByTagName("tr");
+
+  // // Loop through all table rows, and hide those who don't match the search query
+  // for (i = 0; i < tr.length; i++) {
+  //   td = tr[i].getElementsByTagName("td")[2];
+  //   if (td) {
+  //     if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+  //       tr[i].style.display = "";
+  //     } else {
+  //       tr[i].style.display = "none";
+  //     }
+  //   }
+  // }
 }
